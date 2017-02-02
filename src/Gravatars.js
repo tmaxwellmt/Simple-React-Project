6d570387699ef1cadc2553fa3e3c38a1ad92b632
fresh5447 <doug@kosmojo.com>
2017-02-02 14:20:43 -0700

    refactor gravatar to be able to use md5

--- src/Gravatars.js
+++ src/Gravatars.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import md5 from 'md5';
+
+var G_URL = "http://gravatar.com/avatar";
 
 var Gravatars = React.createClass({
   render: function() {
+    var users = WheresOurData.map(function(user){
+      var size = 36;
+      var hash = md5(user.email);
+      var url = G_URL + '/' + hash + '?s=' + size;
+        return <li key={user.id}> <img src={url} width={size}/> {user.name}</li>
+    });
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, Gravatars!</h1>
+        wheres our HTML?
       </div>
     )
   }
