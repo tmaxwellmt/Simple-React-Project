ab0d11342c6ad62fed6db1f19d5dda68e67003dc
fresh5447 <doug@kosmojo.com>
2017-02-02 14:27:20 -0700

    add toggler beginning code

---
+++ src/Toggler.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Toggler = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Someday, I am going to grow up to be a toggler.</h1>
+      </div>
+    )
+  }
+})
+
+export default Toggler;
